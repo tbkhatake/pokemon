fcb44c141f692e4edc6d2ced650d2be53ecbc0a8
salomao <salomaonunespro@gmail.com>
2021-07-20 13:54:06 -0300

    Create map section

--- pages/homepage.php
+++ pages/homepage.php
@@ -2,5 +2,6 @@
 
 <?php get_header() ?>
 
-asdasdasd
+<?php get_template_part( 'template-parts/home/map' ); ?>
+
 <?php get_footer() ?>
---
+++ template-parts/home/map.php
@@ -0,0 +1,11 @@
+<?php 
+    $mapTitle = get_field('titulo_mapa');
+?>
+<section class="map-section">
+    <div class="container">
+        <h2 class="map-title"><?= $mapTitle ?></h2>
+        <div class="map-wrapper">
+            
+        </div>
+    </div>
+</section>
