a8459d6658c71377e5dada693e62bcb9148aebd4
Renan <renanyy18@gmail.com>
2021-07-26 10:45:20 -0300

    :sparkles: Criada a parte desk e mobile da conheça nossos serviços.

--- pages/homepage.php
+++ pages/homepage.php
@@ -2,4 +2,7 @@
 
 <?php get_header() ?>
 
+<?php get_template_part("template-parts/home/know-our-services");?>
+
+
 <?php get_footer() ?>
---
+++ template-parts/home/know-our-services.php
@@ -0,0 +1,19 @@
+<?php $section3 = get_field('section3')?>
+<section class="know-our-services">
+    <h2 class="section-title"><?php echo($section3['section3-title']); ?></h2>
+    <?php foreach($section3['section3-cards'] as $card):?>
+        <div class="know-our-services-cards">
+            <img class="card-img" src="<?=$card['cards-img']['url'];?>" alt="img1">
+            <div class="card-wraper">
+                <h3 class="card-title"><?=$card['cards-title'];?></h3>
+                <span class="card-line"></span>
+                <p class="card-content"><?=$card['cards-content'];?></p>
+                <a class="card-button" href="<?=$card['cards-button-link'];?>"><?=$card['cards-button-text'];?></a>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</section>
+<div class="kos-graphism">
+    <img class="img -bg1" src="<?=get_template_directory_uri()?>/images/fundo-kos1.svg">
+    <img class="img -bg2" src="<?=get_template_directory_uri()?>/images/fundo-kos2.svg">
+</div>
