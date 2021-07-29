2cdae9bd5c6f6608aad89454003d1b78b534e104
Renan <renanyy18@gmail.com>
2021-07-29 11:37:43 -0300

    :sparkles: Criada a parte mobile e desk da Conheça Desktop Super.
    Tambem foram ajustadas:a fonte do titulo da seçao e borda do botao da Conheça nossos serviços e algumas margens do top de outras seçoes.

---
+++ js/home/know-super-desktop.js
@@ -0,0 +1,5 @@
+let slider_ksd = tns({
+    container: '.slider-ksd',
+    "items": 1,
+    controlsContainer: "#customize-controls",
+});
--- template-parts/home/know-our-services.php
+++ template-parts/home/know-our-services.php
@@ -3,7 +3,8 @@
     <h2 class="section-title"><?php echo($section3['section3-title']); ?></h2>
     <?php foreach($section3['section3-cards'] as $card):?>
         <div class="know-our-services-cards">
-            <img class="card-img" src="<?=$card['cards-img']['url'];?>" alt="img1">
+            <img class="card-img" src="<?=$card['cards-img']['url'];?>" alt="imagem">
+            <img class="card-desk-img" src="<?=$card['cards-desk-img']['url'];?>" alt="imagem">
             <div class="card-wraper">
                 <h3 class="card-title"><?=$card['cards-title'];?></h3>
                 <span class="card-line"></span>
@@ -12,6 +13,7 @@
             </div>
         </div>
     <?php endforeach; ?>
+    
 </section>
 <div class="kos-graphism">
     <img class="img -bg1" src="<?=get_template_directory_uri()?>/images/fundo-kos1.svg">
