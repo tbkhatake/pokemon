9dc2adce0fb81d853c630df7ffa4ae99cf2b893e
Renan <renanyy18@gmail.com>
2021-07-30 09:54:54 -0300

    :sparkles: Feita a parte mobile e desk da seçao Contrate-nos.

--- pages/homepage.php
+++ pages/homepage.php
@@ -4,6 +4,6 @@
 <?php get_template_part("template-parts/home/hero");?>
 <?php get_template_part("template-parts/home/self-service");?>
 <?php get_template_part("template-parts/home/know-our-services");?>
+<?php get_template_part("template-parts/home/hire-us");?>
 <?php get_template_part("template-parts/home/know-super-desk");?>
-
 <?php get_footer() ?>
