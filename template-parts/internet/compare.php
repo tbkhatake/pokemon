4fe647fe8577762419af58e39f9b93467dd07e0d
Renan <renanyy18@gmail.com>
2021-08-27 09:15:13 -0300

    :sparkles: Feito as partes mobile e desk da seçao Compare.

--- template-parts/internet/compare.php
+++ template-parts/internet/compare.php
@@ -11,6 +11,7 @@
                             <h3 class="card-plan"><?=$card['plans-plan'];?></h3>
                         </div>
                         <p class="card-benefit">Tecnololgia</p>
+                        <img class="tec-icon" src="<?=$card['plans-tecIcon']['url'];?>" alt="ícone">
                         <p class="card-text -tecnology"><?=$card['plans-tecnology'];?></p>
                         <p class="card-benefit">Velocidade De Download</p>
                         <p class="card-text -downSpeed"><?=$card['plans-downSpeed'];?></p>
@@ -35,56 +36,64 @@
             </div>
         </div>
     </div>
+    
     <div class="compare-desktop">
         <table class="compare-table">
-            <tr>
-                <th>&nbsp;</th>
+            <tr class="tr">
+                <th class="th">&nbsp;</th>
                 <?php foreach($compare as $card):?>
-                    <td> 
+                    <td class="td"> 
                         <div class="card-plan-container"><p class="card-plan"><?=$card['plans-plan'];?></p></div>
                     </td>
                 <?php endforeach; ?>
             </tr>
-            <tr>
-                <th class="benefits-name">Tecnololgia</th>
+            <tr class="tr">
+                <th class="th"> <p class="benefits-name">Tecnololgia</p></th>
                 <?php foreach($compare as $card):?>
-                    <td> <p class="card-text -tecnology"><?=$card['plans-tecnology'];?></p></td>
+                    <td class="td"> 
+                        <img class="tec-icon" src="<?=$card['plans-tecIcon']['url'];?>" alt="ícone">
+                        <p class="card-text -tecnology"><?=$card['plans-tecnology'];?></p>
+                    </td>
                 <?php endforeach; ?>
             </tr>
-            <tr>
-                <th class="benefits-name">Velocidade De Download</th>
+            <tr class="tr">
+                <th class="th"> <p class="benefits-name">Velocidade De Download</p></th>
                 <?php foreach($compare as $card):?>
-                    <td> <p class="card-text -downSpeed"><?=$card['plans-downSpeed'];?></p></td>
+                    <td class="td"> 
+                        <p class="card-text -downSpeed"><?=$card['plans-downSpeed'];?></p>
+                        <img class="down-icon" src="<?=get_template_directory_uri()?>/images/question-circle.png" alt="">
+                        <p class="info-box"><?=$card['plans-obs'];?></p>
+                    </td>
                 <?php endforeach; ?>
             </tr>
-            <tr>
-                <th class="benefits-name">Velocidade De Upload</th>
+            <tr class="tr">
+                <th class="th"> <p class="benefits-name">Velocidade De Upload</p></th>
                 <?php foreach($compare as $card):?>
-                    <td> <p class="card-text -upSpeed"><?=$card['plans-upSpeed'];?></p></td>
+                    <td class="td"> <p class="card-text -upSpeed"><?=$card['plans-upSpeed'];?></p></td>
                 <?php endforeach; ?>
             </tr>
-            <tr>
-                <th class="benefits-name">Wi-Fi</th>
+            <tr class="tr">
+                <th class="th"> <p class="benefits-name">Wi-Fi</p></th>
                 <?php foreach($compare as $card):?>
-                    <td> <p class="card-text -wifi"><?=$card['plans-wifi'];?></p></td>
+                    <td class="td"> <p class="card-text -wifi"><?=$card['plans-wifi'];?></p></td>
                 <?php endforeach; ?>
             </tr>
-            <tr>
-                <th class="benefits-name">Acesso Simultâneo</th>
+            <tr class="tr">
+                <th class="th"> <p class="benefits-name">Acesso Simultâneo</p></th>
                 <?php foreach($compare as $card):?>
-                    <td> <p class="card-text -qttAcess"><?=$card['plans-qttAcess'];?></p></td>
+                    <td class="td"> <p class="card-text -qttAcess"><?=$card['plans-qttAcess'];?></p></td>
                 <?php endforeach; ?>
             </tr>
-            <tr>
-                <th class="benefits-name">Franquia</th>
+            <tr class="tr">
+                <th class="th"> <p class="benefits-name">Franquia</p></th>
                 <?php foreach($compare as $card):?>
-                    <td> <p class="card-text -franchise"><?=$card['plans-franchise'];?></p></td>
+                    <td class="td"> <p class="card-text -franchise"><?=$card['plans-franchise'];?></p></td>
                 <?php endforeach; ?>
             </tr>
-            <tr>
-                <th class="benefits-name">Serviços digitais</th>
+            <tr class="tr">
+                <th class="th"> <p class="benefits-name">Serviços digitais</p></th>
                 <?php foreach($compare as $card):?>
-                    <td> 
+                    <td class="td td-services"> 
                         <div class="card-text services">
                         <?php foreach($card['plans-services'] as $icon):?>
                             <img class="card-icon" src="<?=$icon['plans-services-icon']['url'];?>" alt="ícone">
@@ -93,39 +102,6 @@
                     </td>
                 <?php endforeach; ?>
             </tr>
-            
-            
-
         </table>
     </div>
 
-    <!-- <div class="compare-desktop">
-        <div class="benefits">
-            <p class="benefits-name">Tecnololgia</p>
-            <p class="benefits-name">Velocidade De Download</p>
-            <p class="benefits-name">Velocidade De Upload</p>
-            <p class="benefits-name">Wi-Fi</p>
-            <p class="benefits-name">Acesso Simultâneo</p>
-            <p class="benefits-name">Franquia</p>
-            <p class="benefits-name">Serviços digitais</p>
-        </div>
-        <div class="cards">
-            <?php foreach($compare as $card):?>
-                <div class="card">
-                    <h3 class="card-plan"><?=$card['plans-plan'];?></h3>
-                    <p class="card-text -tecnology"><?=$card['plans-tecnology'];?></p>
-                    <p class="card-text -downSpeed"><?=$card['plans-downSpeed'];?></p>
-                    <p class="card-text -upSpeed"><?=$card['plans-upSpeed'];?></p>
-                    <p class="card-text -wifi"><?=$card['plans-wifi'];?></p>
-                    <p class="card-text -qttAcess"><?=$card['plans-qttAcess'];?></p>
-                    <p class="card-text -franchise"><?=$card['plans-franchise'];?></p>
-                    <div class="card-services">
-                        <?php foreach($card['plans-services'] as $icon):?>
-                            <img class="card-icon" src="<?=$icon['plans-services-icon']['url'];?>" alt="ícone">
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div> -->
-</section>
