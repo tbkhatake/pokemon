3dd986b0af2a149d840c159726242a52e039d41b
Renan <renanyy18@gmail.com>
2021-08-30 16:34:41 -0300

    :sparkles: Criada a parte Desk e Mobile da seçao internetCarousel.

--- pages/internet.php
+++ pages/internet.php
@@ -4,5 +4,5 @@
 <?php get_template_part("template-parts/common/hero");?>
 <?php get_template_part("template-parts/common/plans");?>
 <?php get_template_part("template-parts/internet/expositor");?>
-
+<?php get_template_part("template-parts/internet/know-our-services");?>
 <?php get_footer() ?>
