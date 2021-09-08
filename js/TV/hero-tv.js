96ad6a5c6159d6ac0c4fc4a0f5da55747675189c
salomao <salomaonunespro@gmail.com>
2021-09-08 16:16:30 -0300

    :sparkles: Create TV Carousel

---
+++ js/TV/hero-tv.js
@@ -0,0 +1,7 @@
+let haveHeroTv = document.querySelector('.slider-engine');
+if(haveHeroTv){
+    let slider = tns({
+        container: '.slider-engine',
+        preventScrollOnTouch: 'auto'
+    });
+}
---
+++ pages/tv.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Página TV */ ?>
+
+<?php get_header() ?>
+
+<?php get_template_part("template-parts/TV/hero-tv");?>
+
+
+<?php get_footer() ?>
