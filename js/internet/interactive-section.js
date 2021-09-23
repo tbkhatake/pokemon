b8465fea125c2a4cfb684a9cf0b3d6a4994f2ed6
Renan <renanyy18@gmail.com>
2021-09-23 18:15:13 -0300

    :sparkles: Criada a seçao channels package em desk e mobile.

--- js/internet/interactive-section.js
+++ js/internet/interactive-section.js
@@ -1,21 +1,13 @@
-let slide = document.getElementById('onDesktop');
-let card = document.querySelector('input[name=checkbox]');
+let haveInteractive = document.querySelector('#onDesktop');
+    if(haveInteractive){
+    let slide = document.getElementById('onDesktop');
+    let card = document.querySelector('input[name=checkbox]');
 
-
-
-card.addEventListener("change", function() {
-    if (this.checked) {
-        slide.classList.add("active");
-    }else {
-        slide.classList.remove("active");
-    }
-});
-    
-
-
-
-
-
-
-
-
+    card.addEventListener("change", function() {
+        if (this.checked) {
+            slide.classList.add("active");
+        }else {
+            slide.classList.remove("active");
+        }
+    });
+}
--- pages/tv.php
+++ pages/tv.php
@@ -3,6 +3,6 @@
 <?php get_header() ?>
 
 <?php get_template_part("template-parts/TV/hero-tv");?>
-
+<?php get_template_part("template-parts/TV/channels-packages");?>
 
 <?php get_footer() ?>
