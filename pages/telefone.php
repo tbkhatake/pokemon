e65ddec2c1aed6b240071bc11712e63cdf3e3049
salomao <salomaonunespro@gmail.com>
2021-09-28 17:07:56 -0300

    :sparkles: Add page hero

---
+++ pages/telefone.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Telefone */ ?>
+
+<?php get_header() ?>
+<?php get_template_part("template-parts/common/hero");?>
+
+<?php get_template_part("template-parts/internet/contact");?>
+
+<?php get_footer() ?>
