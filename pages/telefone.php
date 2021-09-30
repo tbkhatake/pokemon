da10c8d49f72e2a40d080fd871805f6caade174e
salomao <salomaonunespro@gmail.com>
2021-09-30 13:57:06 -0300

    :sparkles: Create user path section

--- pages/telefone.php
+++ pages/telefone.php
@@ -2,6 +2,7 @@
 
 <?php get_header() ?>
 <?php get_template_part("template-parts/common/hero");?>
+<?php get_template_part("template-parts/telefone/user-path");?>
 <?php get_template_part("template-parts/telefone/text-block");?>
 <?php get_template_part("template-parts/telefone/contact");?>
 
---
+++ template-parts/telefone/user-path.php
@@ -0,0 +1,9 @@
+<?php 
+    $section_title = get_field('titulo_texto');
+?>
+<section class="path-section">
+    <div class="container">
+        <h2 class="section-title"><?= $section_title; ?></h2>
+        
+    </div>
+</section>
