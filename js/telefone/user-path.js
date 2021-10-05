f83a9366716042467b93dfdd729bb184c5077e47
salomao <salomaonunespro@gmail.com>
2021-10-05 16:08:58 -0300

    :fire: update user-path section

--- js/telefone/user-path.js
+++ js/telefone/user-path.js
@@ -1,13 +1,9 @@
 let taxOff = document.getElementById('dropdownContent');
 
-
-
-
 if (taxOff) {
-let taxBtn = document.getElementById('clickBtn');
-
-taxBtn.addEventListener("click", () => {
-    taxOff.classList.toggle('accordion-content-active');
-  
-})
+    let taxBtn = document.getElementById('clickBtn');
+    taxBtn.addEventListener("click", () => {
+        taxOff.classList.toggle('accordion-content-active');
+    
+    })
 }
