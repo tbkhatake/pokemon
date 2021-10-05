d65fa31c20656341f75988f09c03f4a6635074a9
salomao <salomaonunespro@gmail.com>
2021-10-05 14:35:01 -0300

    :fire: Set graphisms

--- pages/telefone.php
+++ pages/telefone.php
@@ -2,6 +2,8 @@
 
 <?php get_header() ?>
 <?php get_template_part("template-parts/common/hero");?>
+<?php get_template_part("template-parts/common/plans");?>
+<?php get_template_part("template-parts/telefone/know-our-services");?>
 <?php get_template_part("template-parts/telefone/text-block");?>
 <?php get_template_part("template-parts/telefone/contact");?>
 
