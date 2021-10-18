9826479c5bbb24e32be45af64dfdc0e16f7697ad
salomao <salomaonunespro@gmail.com>
2021-10-18 13:08:31 -0300

    :fire: Delete comment faq page

--- pages/politicas.php
+++ pages/politicas.php
@@ -31,46 +31,6 @@
                     </div>
                 <?php  endwhile; ?>
             <?php endif; ?>
-            <!--
-            <div class="faq__item">
-                <div class="ask">
-                    <div class="ask__text">Quais dados pessoais coletamos e porque</div>
-                </div>
-                <div class="answer--border">
-                    <div class="answer">Comentários <br> Quando os visitantes deixam comentários no site, coletamos os
-                        dados
-                        mostrados no formulário de comentários, além do endereço de IP e de dados do navegador do
-                        visitante,
-                        para auxiliar na detecção de spam. <br> Uma sequência anonimizada de caracteres criada a partir
-                        do seu
-                        e-mail (também chamada de hash) poderá ser enviada para o Gravatar para verificar se você usa o
-                        serviço. A política de privacidade do Gravatar está disponível aqui:
-                        https://automattic.com/privacy/. Depois da aprovação do seu comentário, a foto do seu perfil
-                        fica
-                        visível publicamente junto de seu comentário.
-                    </div>
-                </div>
-            </div>
-            <div class="faq__item">
-                <div class="ask">
-                    <div class="ask__text">Quais dados pessoais coletamos e porque</div>
-                </div>
-                <div class="answer--border">
-                    <div class="answer">Comentários <br> Quando os visitantes deixam comentários no site, coletamos os
-                        dados
-                        mostrados no formulário de comentários, além do endereço de IP e de dados do navegador do
-                        visitante,
-                        para auxiliar na detecção de spam. <br> Uma sequência anonimizada de caracteres criada a partir
-                        do seu
-                        e-mail (também chamada de hash) poderá ser enviada para o Gravatar para verificar se você usa o
-                        serviço. A política de privacidade do Gravatar está disponível aqui:
-                        https://automattic.com/privacy/. Depois da aprovação do seu comentário, a foto do seu perfil
-                        fica
-                        visível publicamente junto de seu comentário.
-                    </div>
-                </div>
-            </div>
-            -->
         </div>
     </section>
 <?php get_footer() ?>
